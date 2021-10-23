1fe0f1c87bf74fa9f8367000c18cb0e256a6d529
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-10-23 19:52:29 -0300

    Show messages using socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { api } from '../../services/api';
+import io from 'socket.io-client';
 
 import styles from './styles.module.scss';
 
@@ -13,8 +14,27 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+socket.on('new_message', (newMessage: Message) =>
+  messagesQueue.push(newMessage)
+);
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
